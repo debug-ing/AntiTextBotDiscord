b077de5145a20b6a1ec149c27f0a9dfb77aa7c84
M4HDI <mahdicsharp0098@gmail.com>
2021-01-13 00:22:19 +0330

    add Controller and Off On

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 const Discord = require('discord.js');
 const Client = new  Discord.Client();
-var cars = ["", "", ""];
+var cars = ["kos", "kir", "kon"];
 
 var s = 0;
+
+//
+
 
 Client.once('ready', () => {
     console.log('Iam ready');
 });
 
 Client.on('message', msg => {
-    if(msg.content == "start") {
-        s = 1;
-        msg.channel.send('bot is started');
-    }else{
-        if(s === 1){ 
-            for(var i = 0 ; i <cars.length;i++){
-                var state = msg.content.includes(cars[i]);
-                
-                if(state === false){
-    
-                }else{  
-                    msg.channel.send("ff");
-                    return;
-                }
-            }
-        }else{
+    if(msg.content === "start") {
+        if(msg.member.roles.cache.some(role => role.name === 'IDI') === true){
+            s = 1;
+            msg.channel.send('bot is started');
         }
-         
+    }else if (msg.content === "stop") {
+        if(msg.member.roles.cache.some(role => role.name === 'IDI') === true){
+            s = 0;
+            msg.channel.send('bot is Stoped');
+        }
+    }else{
+        if(msg.member.roles.cache.some(r => r.name === "IDI")){
+        }else{
+            if(s === 1){
+                for(var i = 0 ; i <cars.length;i++){
+                    var state = msg.content.includes(cars[i]);
+
+                    if(state === false){
+
+                    }else{
+                        msg.channel.send("ff");
+                        return;
+                    }
+                }
+            }else{
+            }
+        }
     }
     
 });
 
 
 
-Client.login('teken');
+Client.login('');
 
